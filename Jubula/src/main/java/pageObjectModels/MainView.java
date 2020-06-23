fb5962d42006a94272db656b6203c9ef342251ed
benjaminmmiller <benmarkmiller@gmail.com>
2020-06-23 12:29:27 -0400

    Forgot to stage some files

---
+++ Jubula/src/main/java/pageObjectModels/MainView.java
@@ -0,0 +1,18 @@
+package pageObjectModels;
+
+import org.eclipse.jubula.toolkit.swt.internal.impl.TabComponent;
+
+public class MainView extends BaseRCPView{
+
+	
+	
+	public MainView() {
+		
+		TabComponent tab;
+		
+	}
+	
+	
+	
+	
+}
